bc59d9b918d076b6e932c61185d72561e64cca28
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-30 17:55:55 +0700

    "Xu ly logic"

--- app/src/main/java/com/ptp/phamtanphat/random1206/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/random1206/MainActivity.java
@@ -3,32 +3,43 @@ package com.ptp.phamtanphat.random1206;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtSomin,edtSomax ;
+    Button btnRandom ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Random random = new Random();
+        edtSomax = findViewById(R.id.edittextSomax);
+        edtSomin = findViewById(R.id.edittextSomin);
+        btnRandom = findViewById(R.id.buttonRandom);
 
-        //Bieu thuc su dung toan tu cong thi se hieu rang bieu thuc noi chuoi voi nhau
-//        0 10;
-//        5 -10;
-//        0 - (kc - 1);
-//        String ketqua = random.nextInt(16) + 5 + "";
+        btnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String textsomin = edtSomin.getText().toString();
+                String textsomax = edtSomax.getText().toString();
+
+                int somin = Integer.valueOf(textsomin);
+                int somax = Integer.valueOf(textsomax);
+
+                if (somin >= somax){
+                    somax = somin + 1;
+                }
 
 
-        for (int i = 0 ; i < 20 ; i++){
-            String ketqua2 = random.nextInt(15) + 5 + "";
-            Log.d("BBB",ketqua2);
-        }
-//        Toast.makeText(this, ketqua , Toast.LENGTH_SHORT).show();
-//
-//        Log.d("BBB",ketqua2);
+            }
+        });
+
     }
 }
